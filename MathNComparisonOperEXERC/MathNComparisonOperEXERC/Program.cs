c7190bfc3c01493903d37b02af8400b0d9a8a978
Luke Sheedy <thesheedy@hotmail.com>
2019-12-05 15:36:49 -0800

    Finished  exersice 67
    Made a Salery Comparieson program

---
+++ MathNComparisonOperEXERC/MathNComparisonOperEXERC/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace MathNComparisonOperEXERC
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.ReadLine();
+
+            Console.WriteLine("Hourly Rate?");
+            int userhourly = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Hours worked per week?");
+            int weekhourly = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Person 2");
+            Console.ReadLine();
+
+            Console.WriteLine("Hourly Rate?");
+            int userhourly2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Hours worked per week?");
+            int weekhourly2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Weekly salary of Person 1:");
+            int combined = userhourly * weekhourly;
+            Console.WriteLine(combined);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Weekly salary of Person 2:");
+            int combined2 = userhourly2 * weekhourly2;
+            Console.WriteLine(combined2);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(combined>combined2);
+            Console.ReadLine();
+
+            Console.WriteLine("Thanks For Comparing!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
