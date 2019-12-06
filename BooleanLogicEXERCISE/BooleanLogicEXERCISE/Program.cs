18a460cc21a4247b2149c8346bdc2f48a12b8e5e
Luke Sheedy <thesheedy@hotmail.com>
2019-12-05 17:05:26 -0800

    Updated Boolean Code
    finished coding for the boolean program for if quilfied

---
+++ BooleanLogicEXERCISE/BooleanLogicEXERCISE/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace BooleanLogicEXERCISE
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int userAGE = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? (please answer true or false)");
+            string submit = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int userticket = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.ReadLine();
+            Console.WriteLine(userAGE > 15 && submit == "false" && 3 > userticket);
+            Console.ReadLine();
+
+        }
+    }
+}
