b3e3a851b3da46abd2cd70eb8b19a59336f71916
Luke Sheedy <thesheedy@hotmail.com>
2019-12-09 12:40:35 -0800

    Finsihed Up Strings
    Finished up learning about Strings

---
+++ StringExercise/StringExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "The mouse in the house ";
+            string two = "went under the bed ";
+            string three = "to find if there was cheese.";
+            Console.WriteLine(one+ two + three);// Concatenate three strings
+
+            one = one.ToUpper();
+            Console.WriteLine(one);// upper case whole string
+
+            StringBuilder Test = new StringBuilder();
+            Test.Append("Building some paragrpahs is the key, ");
+            Test.Append("to make sure we can test ");
+            Test.Append("our knowledge on Stringbuilder exercise. ");
+            Console.WriteLine(Test);// Building a pharagraph one at a time taking the orignal and adding to it
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
