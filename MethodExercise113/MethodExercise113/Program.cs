50ba0958d1a35168739a3ee3d3db375a595993a1
Luke Sheedy <thesheedy@hotmail.com>
2019-12-20 13:45:20 -0800

    Finished with help on Exercise 113
    was able to figure out what voided was

---
+++ MethodExercise113/MethodExercise113/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoInt Sum = new TwoInt();
+            Sum.Two(2, 5);// before listing the parameters by name
+            Sum.Two(var2: 5, var: 2);// after listing by name
+
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+}
---
+++ MethodExercise113/MethodExercise113/TwoInt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise113
+{
+    class TwoInt
+    {
+
+    public void Two(int var, int var2 )
+    {
+            var = var * 8;
+            Console.WriteLine(var2);
+
+    }
+
+
+    }
+}
