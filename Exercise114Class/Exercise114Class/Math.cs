b573cba589e533924534d82bdc9ed3dc4f2d5370
Luke Sheedy <thesheedy@hotmail.com>
2019-12-20 17:10:53 -0800

    Finished With Exercise 114
    Finished coding exercise 114 with alans help!

---
+++ Exercise114Class/Exercise114Class/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise114Class
+{
+    class Math
+    {
+        public int Multi(int num1)
+        {
+            num1 = num1 * 9;
+            return num1;
+        }
+
+        public int Multi(decimal num2)// Paramiters are the inside of ()
+        {
+            num2 = num2 / 9;
+            int num3 = Decimal.ToInt32(num2);
+            return num3;
+        }
+
+        public int Multi(string num3)//return only return type thing oustide (), but can accept many data inside input parameters()
+        {
+            
+            int num4 = Convert.ToInt32(num3);
+            num4 = num4 + 9;
+            return num4;
+        }
+
+    }
+}
---
+++ Exercise114Class/Exercise114Class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise114Class
+{
+    class Program
+    {
+        static void Main(string[] args)// will start with this method
+        {
+            Math math = new Math();
+            int Answer = math.Multi(12);//
+            int Answer2 = math.Multi(12m);// are the arguments givin to the methods
+            int Answer3 = math.Multi("12");
+                
+            Console.WriteLine(Answer);
+            Console.WriteLine(Answer2);
+            Console.WriteLine(Answer3);
+            Console.ReadLine();
+
+        }
+    }
+}
